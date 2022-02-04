90cbc7d6c5136a51e8a20814d19e50bc5ad4f395
SakshiS2609 <88336665+SakshiS2609@users.noreply.github.com>
2022-02-04 12:04:15 +0530

    First eclipse program

---
+++ corejava/src/corejava/main/Display.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package corejava.main;
+
+/**
+ * @author HP
+ *
+ */
+public class Display {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hiiii");
+	}
+
+}
